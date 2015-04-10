8f1c9079a74585dd3d45aa2f4f63ca10e2f0fcf8
ThunderShiviah <thunder@uoregon.edu>
2015-04-09 20:44:03 -0700

    changed folders

---
+++ tests/brainmix_register_tests.py
@@ -0,0 +1,18 @@
+from nose.tools import *
+import brainmix_register
+
+def setup():
+    print("SETUP!")
+
+def teardown():
+    print("TEAR DOWN!")
+
+def test_basic():
+    print("I RAN!")
+
+def test_list_of_two_images()
+    """ A test to check if the registration function in main can run a list of two ndarray images.
+
+    input: a list of two ndarray images
+    """
+    pass
