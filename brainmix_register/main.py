4c0e977cd3ba9016f5c99dd8506b3898eae6c59f
Thunder Shiviah <thunder@uoregon.edu>
2015-04-13 11:36:47 -0700

    Added testing method that takes in two images, converts them to a list, and runs them through the pipeline.

--- brainmix_register/main.py
+++ brainmix_register/main.py
@@ -1,22 +1,42 @@
 from register_methods import * #I need to change this so I'm not wildcard importin
 from pipeline import pipeline
 import numpy as np
+### For Testing ###
+from skimage import io
+from skimage.color import rgb2gray
+
+from matplotlib import pyplot as plt
+
 
 """The transform pipeline outputs a transform"""
 register_pipeline  = pipeline(
         downsample_image,
-        subtract_background,
-        create_binary,
+        #subtract_background,
+        #create_binary,
         orb_extractor,
         estimate_transform
         ) 
 
-        
+
 def registration(stack):
     # TODO: put in a good docstring. I need to figure out what the input type on stack is.
-     assert isinstance(stack, list) == True, "stack is not a list: %r" % stack
+    assert isinstance(stack, list) == True, "stack is not a list: %r" % stack
 
     transform = register_pipeline(stack)
-    # show_images(apply_transform(stack, transform)) 
+    show_images(apply_transform(stack, transform)) 
     return transform
-    
+
+if __name__ == "__main__":
+
+    file1 = '../data/p1-D1-01b.jpg'
+    file2 = '../data/p1-F4-01b.jpg'
+
+    ''' Read files and convert to grey'''
+    img1 = io.imread(file1 ,as_grey=True) # query image
+    img2 = io.imread(file2 ,as_grey=True) #training image
+
+    '''convert images to stack'''
+    stack = [img1, img2]
+    assert stack[0].shape == stack[1].shape
+
+    registration(stack)
