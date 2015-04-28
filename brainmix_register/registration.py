51b3f1995ffc431b48b007d1dfb8387bd9313576
Thunder Shiviah <thunder@uoregon.edu>
2015-04-28 12:08:56 -0700

    fixed some PEP8 style errors

--- brainmix_register/registration.py
+++ brainmix_register/registration.py
@@ -1,42 +1,43 @@
-import sys, os, glob
+import os, glob
 import numpy as np
-sys.path.append("../Modules/")
-import skimage
+from skimage import feature
 from skimage import io
-
-from skimage import data
 from skimage import transform as tf
-from skimage.feature import (match_descriptors, corner_harris,
-                             corner_peaks, ORB, plot_matches)
-from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
-from skimage.color import rgb2gray, gray2rgb
+from skimage.color import gray2rgb
 
 
-#-----------------Registration function-------------------------
+# -----------------Registration function-------------------------
+
 
 def reg(src, dst):
-    """Takes in a source and destination image and returns a registered source image.
+    """Takes in a source and destination image and returns a
+    registered source image.
     """
-    shifts, err, phasediff = skimage.feature.register_translation(src, dst) 
+    shifts, err, phasediff = feature.register_translation(src, dst)
     tform = tf.AffineTransform(translation=shifts)
-    reg_dst =tf.warp(dst, inverse_map=tform.inverse) 
+    reg_dst = tf.warp(dst, inverse_map=tform.inverse)
 
     return reg_dst
 
-#----------------Stack function --------------------------------
+# ----------------Stack function --------------------------------
+
 
 def reg_iter(stack):
-    """Takes a stack of images and registers each one using the first image as the destination. 
+    """
+    input: a stack of images and registers each one using the first
+    image as the destination.
     output: a registered stack.
     """
     dst = stack[0]
     reg_stack = [reg(src, dst) for src in stack]
     return reg_stack
 
-#--------------View overlayed result--------------------------
+# --------------View overlayed result--------------------------
+
+
 def overlay_pics(src, dst):
-    
+
     image0_ = src
     image1_ = dst
 
@@ -63,34 +64,37 @@ def overlay_pics(src, dst):
     merged /= np.maximum(alpha, 1)[..., np.newaxis]
     return merged
 
-#---------------main-------------------------------
+# ---------------main-------------------------------
+
+
 def registration(img_stack):
     """input: ndarray of images
     ouput: ndarray of registered images
     """
     reg_stack = reg_iter(img_stack)
-    #assert np.array_equal(img_stack[0], reg_stack[0]) #dst images should be the same 
+
     if not np.array_equal(img_stack[0], reg_stack[0]):
         print("destination image has been changed in registered stack.")
-    assert not np.array_equal(img_stack[0], reg_stack[1]) #src and dst images should be different
+    # src and dst images should be different
+    assert not np.array_equal(img_stack[0], reg_stack[1])
     return io.concatenate_images(reg_stack)
 
 if __name__ == "__main__":
-    #------------------Create input ndarray------------------------
+    # ------------------Create input ndarray------------------------
     inputDir = '../data/test/'
     imageFiles = glob.glob(os.path.join(inputDir, '*.jpg'))
     imageVolume = io.ImageCollection(imageFiles, as_grey=True).concatenate()
     stack = imageVolume
 
     reg_stack = registration(stack)
-  
+
     print('stack is of type', type(stack))
     print('stack dimensions are', stack.shape)
     print('registered stack is of type', type(reg_stack))
     print('registered stack dimensions are', reg_stack.shape)
 
     merged = [overlay_pics(stack[0], img) for img in stack]
-    merged_reg = [overlay_pics(reg_stack[0], img) for img in reg_stack]  
+    merged_reg = [overlay_pics(reg_stack[0], img) for img in reg_stack]
     fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(8, 5))
 
     plt.gray()
