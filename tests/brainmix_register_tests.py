66b434a46dc40b861ad1364a6e77b8dc6932b5be
Thunder Shiviah <thunder@uoregon.edu>
2015-05-17 21:11:22 -0700

    Cleaned up some test cases. Started working on a setup function.

--- tests/brainmix_register_tests.py
+++ tests/brainmix_register_tests.py
@@ -1,41 +1,27 @@
+import os, glob
 from nose.tools import *
-import brainmix_register
+import brainmix_register.registration as reg
 import skimage
+from skimage import io
 
+# ------------------Create input ndarray------------------------
+# TODO: Convert this into a set-up decorator.
 def setup():
-    print("SETUP!")
+    inputDir = '../data/test/'
+    imageFiles = glob.glob(os.path.join(inputDir, '*.jpg'))
+    #imageVolume = io.ImageCollection(imageFiles, as_grey=True).concatenate()
+    #stack = imageVolume
+    return imageFiles
+
+
+
 
 def teardown():
     print("TEAR DOWN!")
 
 def test_basic():
+    imageFiles = setup()
     print("I RAN!")
-
-def try_registration(stack):
-    try:
-        brainmix_register.main(registration)
-    except:
-        print("Registration on {stack_type failed}").format(stack_type=type(stack))
-
- 
-def test_list_of_two_images():
-    """ A test to check if the registration function in main can run a list of two ndarray images.
-
-    input: a list of two ndarray images
-    """
-    file1 = "../data/p1-D1-01b.jpg"
-    file2 = "../data/p1-F4-01b.jpg"
-    
-    """ Read files and convert to grey """
-    img1 = skimage.io.imread(file1, as_gray=True) #query image
-    img2 = skimage.io.imread(file2, as_gray=True) #training image
-    
-    stack = [img1, img2]
+    print(imageFiles)
 
 
-def check_that_stack_is_same_size(stack):
-    """ Checks that every image in the stack has the same dimensions
-    input: a stack of images
-    """
-    for image in stack:
-        assert image.shape == stack[0].shape
